e4514501c549cb0a4b6387abfe3bcc33fde8bb5e
Jeon Hwasoo <royallhs@naver.com>
2024-02-27 19:26:55 +0900

    PUBLIC_URL 추가

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import Tt from './views/tt/Tt';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={<Tt />} />
                 <Route path="/temp" element={<Temp1 />} />

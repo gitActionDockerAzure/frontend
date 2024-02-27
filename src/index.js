ddbd78cb10b5f6707b67c0e8ea7f78e12c35617b
Hwasoo-Jeon <royallhs@naver.com>
2024-02-27 23:19:37 +0900

    리디렉션 삭제

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import Temp1 from './views/main/Temp1';
-import Tt from './views/tt/Tt';
-import Temp2 from './views/main/Temp2';
+import Temp1 from "./views/main/Temp1";
+import Tt from "./views/tt/Tt";
+import Temp2 from "./views/main/Temp2";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <Routes>
-                <Route path="/" element={<Navigate replace to="/temp" />} />
-                <Route path="/temp" element={<Temp1 />} />
-                <Route path="/app" element={<App />} />
-                <Route path="/temp2" element={<Temp2 />} />
-            </Routes>
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Temp1 />} />
+        <Route path="/app" element={<App />} />
+        <Route path="/temp2" element={<Temp2 />} />
+        <Route path="/tt" element={<Tt />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
